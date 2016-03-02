db44babe4ca8d089924c517dfcec7a98063c8d43
Hans Chun <hanschun@gmail.com>
2016-03-02 08:07:16 -0800

    updating index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import makeStore from '../src/store';
 import Router, {Route} from 'react-router';
-// import {createStore} from 'redux';
-import createHistory from 'history/lib/createHashHistory';
-// import {Provider} from 'react-redux';
-// import reducer from './reducer';
 import locations from './locations';
-// const store = createStore(reducer);
-// store.dispatch({
-//   type: 'SET_STATE',
-//   state: {
-//       locations: locations
-//   }
-// });
-// import {setState} from './action_creators';
-// import App from './components/App';
 import Panel from './components/Panel';
 import GMap from './components/GMap';
 import routes from './routes';
