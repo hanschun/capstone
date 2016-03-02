749494b9a270ff91aa4beb1257862f03c45dc95e
Hans Chun <hanschun@gmail.com>
2016-03-02 08:43:55 -0800

    removing unnecessary dependencies

--- src/components/GoBtn.jsx
+++ src/components/GoBtn.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
-import Button from 'react-bootstrap/lib/Button';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
-    return <Button id="go">Go</Button>;
+    return <button id="go">Go</button>;
   }
 });
