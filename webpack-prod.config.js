bef1605a71b62e5e9da2de89089c5e35bcc67628
Hans Chun <hanschun@gmail.com>
2016-03-01 21:38:11 -0800

    Updating production config

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,22 +1,28 @@
 var webpack = require('webpack');
-
+var path = require('path');
 module.exports = {
   devtool: 'source-map',
   entry: [
     './src/index'
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'react-hot!babel'
-    }]
+    loaders: [
+      { test: /\.jsx$/,
+             loader: 'babel',
+             include: path.join(__dirname, 'src') },
+           { test: /\.js$/,
+             loader: 'babel',
+             exclude: /node_modules/ },
+           { test: /\.scss?$/,
+             loader: 'style!css!sass',
+             include: path.join(__dirname, 'css') },
+           ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   output: {
-    path: __dirname + '/dist',
+    path: path.join(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
