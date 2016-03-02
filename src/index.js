48e7c64bbc2c9bb803b09ec00b57d028717794a6
Hans Chun <hanschun@gmail.com>
2016-03-02 08:50:57 -0800

    updating react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router, {Route} from 'react-router';
-import createHistory from 'history/lib/createHashHistory';
+import { Router, Route, browserHistory } from 'react-router';
 import locations from './locations';
 import Panel from './components/Panel';
 import GMap from './components/GMap';
 import routes from './routes';
 
 ReactDOM.render(
-  <Router history={createHistory({ queryKey: false })} onUpdate={() => window.scrollTo(0, 0)}>
+  <Router history={browserHistory} onUpdate={() => window.scrollTo(0, 0)}>
   {routes}
   </Router>,
   document.getElementById('app')
