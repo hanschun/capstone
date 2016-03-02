d7ada48cb603fc7a47f3cf28b05529754cd9316f
Hans Chun <hanschun@gmail.com>
2016-03-02 09:55:50 -0800

    Updating list view

--- src/components/ListBtn.jsx
+++ src/components/ListBtn.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
+import { Link } from 'react-router';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
-    return <a href="#/list"><button id="tolist">List View</button></a>;
+    return <Link to="/list"><button id="tolist">List View</button></Link>;
   }
 });
